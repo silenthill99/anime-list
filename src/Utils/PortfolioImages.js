edf229fcea88b736955ee5dc0d539d6bb470fdc8
BuildTools <unconfigured@null.spigotmc.org>
2024-01-19 16:54:04 +0100

    Omtimisation du portfolio, avec le rajout d'une liste

---
+++ src/Utils/PortfolioImages.js
@@ -0,0 +1,26 @@
+export const PortfolioImages = [
+    {
+        name: "K-on!",
+        image: "k-on_1039492.jpg"
+    },
+    {
+        name: "No game No life",
+        image: "No game no life.jpg"
+    },
+    {
+        name: "Suzume",
+        image: "suzume-film.jpg"
+    },
+    {
+        name: "Bocchi the rock",
+        image: "bocchi-the-rock-hitori-gotou.gif"
+    },
+    {
+        name: "Le voyage de Chihiro",
+        image: "le voyage de chihiro.jpg"
+    },
+    {
+        name: "Lost song",
+        image: "lost-song.jpg"
+    }
+]
