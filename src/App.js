5c76eb19ac2136513638d45c6755da2e42d846cf
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 17:48:47 +0100

    Update du projet

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import { SiMyanimelist } from "react-icons/si";
 import {Actualités} from "./Pages/Actualités";
 import { SiCrunchyroll } from "react-icons/si";
 import {Critiques} from "./Pages/Critiques";
+import {Contacts} from "./Pages/Contacts";
 
 function App() {
   return (
     <div>
         <header>
+            <img src="Logo AnimeList.png" width={100} alt="logo"/>
             <nav>
                 <NavLink to="">Accueil</NavLink>
                 <NavLink to="musiques">Musiques</NavLink>
                 <NavLink to="portfolio">Portfolio</NavLink>
                 <NavLink to="news">Actualités</NavLink>
                 <NavLink to="critiques">Critiques</NavLink>
+                <NavLink to="contacts">Nous contacter</NavLink>
             </nav>
         </header>
         <Routes>
@@ -26,6 +29,7 @@ function App() {
             <Route path="portfolio" element={<Portfolio/>}/>
             <Route path="news" element={<Actualités/>}/>
             <Route path="critiques" element={<Critiques/>}/>
+            <Route path="contacts" element={<Contacts/>}/>
         </Routes>
         <footer>
             <a href="https://myanimelist.net/animelist/silenthill99" target="_blank" rel="noreferrer"><SiMyanimelist className="my-anime-list"/></a>
---
+++ src/Pages/Contacts.jsx
@@ -0,0 +1,9 @@
+import "../css/Contacts.scss"
+
+export const Contacts = () => {
+    return(
+        <main>
+            <h1>Nous contacter</h1>
+        </main>
+    )
+}
