8a830ee7de65b31e34b7c96002eac51fcd0f7046
silenthill99 <florian.graziani@sfr.fr>
2024-01-21 23:26:22 +0100

    Rédaction du résulé du film "A silent Voice"

--- src/Pages/Critiques.jsx
+++ src/Pages/Critiques.jsx
@@ -15,11 +15,15 @@ export const Critiques = () => {
                             classe,
                             une jeune fille sourde. Shoko va alors être prise d'affection par tous ces camarades de
                             classe,
-                            sauf Shoya qui va se mettre à harceler et intimider la jeune fille.
+                            sauf Shoya qui va se mettre à harceler et intimider la jeune fille. Ce harcèlement va pousser
+                            la jeune fille à changer d'école, et Shoya passe aussitôt de harceleur à harcelé.<br/><br/>
+                            Cinq ans plus tard, les rumeurs de ce qu'a fait Shoya dans le passé ont fait le tour du lycée.
+                            Shoya a donc désormais le status de paria, a développé une forme de phobie sociale, et a plus
+                            qu'une seule ambition : Retrouver Shoko afin de s'excuser et de réparer les erreurs du passé.
                         </p>
                     </div>
                 </div>
-                <h2>Critique</h2>
+                <h2 className="sv-critique">Critique</h2>
             </section>
         </main>
     )
