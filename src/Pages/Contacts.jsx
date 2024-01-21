f1d199fcaaae4aad448e82bdb01fe6aa9989a011
silenthill99 <florian.graziani@sfr.fr>
2024-01-21 19:11:32 +0100

    Construction du formulaire de connection

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,9 +1,18 @@
 import "../css/Contacts.scss"
+import {useState} from "react";
 
 export const Contacts = () => {
+    const [inputValue, setInputValue] = useState("Nom")
     return(
         <main>
             <h1>Nous contacter</h1>
+            <form className="contact">
+                <input type="text" placeholder="Nom"/>
+                <input type="text" placeholder="Prénom"/><br/>
+                <input type="email" placeholder="Adresse email" className="email"/><br/>
+                <textarea placeholder="Votre message"/>
+                <button type="submit">Envoyer</button>
+            </form>
         </main>
     )
 }
