1d9e11680d4226fa5a2a4640a4db53ebcb4e1b87
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 13:41:54 +0100

    Mise à jour du contenu

--- src/App.js
+++ src/App.js
@@ -10,33 +10,33 @@ import {Critiques} from "./Pages/Critiques";
 import {Contacts} from "./Pages/Contacts";
 
 function App() {
-  return (
-    <div>
-        <header>
-            <img src="Logo AnimeList.png" width={100} alt="logo"/>
-            <nav>
-                <NavLink to="">Accueil</NavLink>
-                <NavLink to="musiques">Musiques</NavLink>
-                <NavLink to="portfolio">Portfolio</NavLink>
-                <NavLink to="news">Actualités</NavLink>
-                <NavLink to="critiques">Critiques</NavLink>
-                <NavLink to="contacts">Nous contacter</NavLink>
-            </nav>
-        </header>
-        <Routes>
-            <Route path="" element={<Home/>}/>
-            <Route path="musiques" element={<Musiques/>}/>
-            <Route path="portfolio" element={<Portfolio/>}/>
-            <Route path="news" element={<Actualités/>}/>
-            <Route path="critiques" element={<Critiques/>}/>
-            <Route path="contacts" element={<Contacts/>}/>
-        </Routes>
-        <footer>
-            <a href="https://myanimelist.net/animelist/silenthill99" target="_blank" rel="noreferrer"><SiMyanimelist className="my-anime-list"/></a>
-            <SiCrunchyroll className="crunchyroll"/>
-        </footer>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <NavLink to=""><img src="Logo AnimeList.png" width={100} alt="logo"/></NavLink>
+                <nav>
+                    <NavLink to="">Accueil</NavLink>
+                    <NavLink to="musiques">Musiques</NavLink>
+                    <NavLink to="portfolio">Portfolio</NavLink>
+                    <NavLink to="news">Actualités</NavLink>
+                    <NavLink to="critiques">Critiques</NavLink>
+                    <NavLink to="contacts">Nous contacter</NavLink>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="" element={<Home/>}/>
+                <Route path="musiques" element={<Musiques/>}/>
+                <Route path="portfolio" element={<Portfolio/>}/>
+                <Route path="news" element={<Actualités/>}/>
+                <Route path="critiques" element={<Critiques/>}/>
+                <Route path="contacts" element={<Contacts/>}/>
+            </Routes>
+            <footer>
+                <a href="https://myanimelist.net/animelist/silenthill99" target="_blank" rel="noreferrer"><SiMyanimelist className="my-anime-list"/></a>
+                <SiCrunchyroll className="crunchyroll"/>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
